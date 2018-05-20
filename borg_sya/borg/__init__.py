a130b36b29f6136a065516c60da4a32a85423514
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2018-05-20 21:54:11 +0200

    Restructure parsing of borgs output, now permits multi-line json

--- borg_sya/borg/__init__.py
+++ borg_sya/borg/__init__.py
@@ -91,32 +91,69 @@ class Borg():
         self._log = log or logging.getLogger('borg')
         self._log_json = 'raw'
 
-    def _readerthread(self, fh, name, buf, condition):
-        for line in fh:
+    def _readerthread(self, fh, name, json, buf, condition):
+        """ Reads either raw lines or JSON objects from the given stream. If
+            reading JSON and the first line starts with an opening brace,
+            subsequent lines will be aggregated until a valid JSON object
+            results. Anything not wrapped in braces will not be considered
+            to be JSON and will be dropped with an entry to the debug log.
+        """
+        def _pass_msg(msg):
             with condition:
                 buf.put((name, line))
                 condition.notify()
-        fh.close()
-        with condition:
-            buf.put((name, POISON))
-            condition.notify()
 
-    def _communicate_linewise(self, p, stdout=True, stderr=True):
+        if json:
+            previous = ""
+            for line in fh:
+                line = previous + line
+                if line.lstrip().startswith('{'):
+                    try:
+                        msg = json.loads(line)
+                    except json.JSONDecodeError:
+                        # Assume that all of the JSON will be well-formed.
+                        # Then, a decoding error simply means that not all of
+                        # the JSON was read yet, i.e. borg has split it over
+                        # multiple lines.
+                        previous = line + '\n'
+                    else:
+                        previous = ""
+                        if self._log_json == 'raw':
+                            # Maybe not a good idea because this might include
+                            # listings with potentially many thousand items
+                            self._log.debug(('[JSON] ' + msg.decode('utf8')).rstrip('\n'))
+                        _pass_msg(msg)
+                else:
+                    # Not JSON
+                    self._log.debug(('[NOT JSON] ' + msg.decode('utf8')).rstrip('\n'))
+                    return None
+                    # _pass_msg(line)
+j       else:
+            for line in fh:
+                _pass_msg(line)
+        fh.close()
+        _pass_msg(POISON)
+
+    def _communicate(self, p, stdout='raw', stderr='raw'):
         buf = Queue()
         nthreads = 0
         threads = []
         new_msg = Condition()
-        if stdout:
+        if stdout in ['raw', 'json']:
             stdout_thread = Thread(target=self._readerthread,
-                                   args=(p.stdout, 'stdout', buf, new_msg)
+                                   args=(p.stdout,
+                                         'stdout', stdout == 'json',
+                                         buf, new_msg),
                                    )
             stdout_thread.daemon = True
             stdout_thread.start()
             nthreads += 1
             threads.append(stdout_thread)
-        if stderr:
+        if stderr in ['raw', 'json']:
             stderr_thread = Thread(target=self._readerthread,
-                                   args=(p.stderr, 'stderr', buf, new_msg)
+                                   args=(p.stderr, 
+                                         'stderr', stderr == 'json',
+                                         buf, new_msg),
                                    )
             stderr_thread.daemon = True
             stderr_thread.start()
@@ -158,12 +195,18 @@ class Borg():
         self._running = True
 
         try:
-            for stdout, stderr in self._communicate_linewise(p):
+            for stdout, stderr in self._communicate(p, stdout='raw',
+                                                    stderr='json'):
                 if output and stdout is not None:
                     outbuf.append(stdout)
                 elif stderr:
-                    msg = self.parse_json(stderr)
                     if msg.get('type') == 'log_message':
+                        if msg.get('msgid') in _ERROR_MESSAGE_IDS:
+                            # Does this always mean that there was a fatal
+                            # error, or would it be sensible to communicate
+                            # this to the outside in a reentrant way?
+                            e = BorgError(**msg)
+                            raise e
                         name = msg.get('name', '')
                         if (name.startswith('borg.output') and name not in
                                 ['borg.output.progress']
@@ -184,28 +227,6 @@ class Borg():
                 self._log.debug(('[JSON OUT] ' + line.decode('utf8')).rstrip('\n'))
 
         return(outbuf)
-
-    def parse_json(self, msg):
-        # TODO: collect multi-line json
-        # TODO: do not accept json that is not wrapped in braces, e.g.
-        # lines like "[foo]\n"
-        if self._log_json == 'raw':
-            # Maybe not a good idea because this might include listings with
-            # potentially many thousand items
-            self._log.debug(('[JSON] ' + msg.decode('utf8')).rstrip('\n'))
-
-        try:
-            msg = json.loads(msg)
-        except json.JSONDecodeError:
-            self._log.debug(('[NOT JSON] ' + msg.decode('utf8')).rstrip('\n'))
-            return None
-
-        if msg.get('type') == 'log_message':
-            if msg.get('msgid') in _ERROR_MESSAGE_IDS:
-                e = BorgError(**msg)
-                raise e
-
-        return msg
 
     @_while_running()
     def _signal(self, sig):
